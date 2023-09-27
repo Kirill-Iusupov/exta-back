fe741ac66c6de5d1e4b7b89a0911be58c31b8839
AprioriDev <apriorifox.dev@gmail.com>
2023-09-27 18:30:14 +0600

    third deploy commit

--- index.js
+++ index.js
@@ -1,55 +1,25 @@
 const express = require('express')
-const mongoose = require("mongoose")
 const cors = require('cors')
 require('dotenv').config()
 
-const MONGOURI = process.env.MONGOURI
 
-const databaseConnect = async () => {
-    try{
-        await mongoose
-        .connect(MONGOURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-        console.log("DB ok");
-    }
-    catch (error) {console.log("DB error => ",error)}
-}
-
-
-databaseConnect()
 
 const app = express()
 
 const PORT = process.env.PORT
 
-
-app.use(cors())
 app.use(express.json({extended: true}))
 
+const connectDB = require("./connectMongo")
+
+connectDB()
 
 
 app.use('/api/items', require("./routes/items.routes"))
 
 
-async function start () {
-    try {
-        await mongoose.connect(MONGOURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModidy: true  
-        })
-        app.listen(PORT, () => console.log("Server has been started on port" + PORT))
-    }
-    catch (error) {
-        console.log("Server error: " + error)
-        process.exit(1)
-    }
-}
-
-start()
-
-
+app.listen(PORT, ()=> {
+    console.log(`Server has been started on server ${PORT}`)
+    } 
+)
 
