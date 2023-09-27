80e8a56e1e40e762ec80cfe3924964e7b6e653ac
AprioriDev <apriorifox.dev@gmail.com>
2023-09-27 18:49:31 +0600

    open uri

--- connectMongo.js
+++ connectMongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGOURI)
+        await mongoose.connect("mongodb+srv://apriorifoxdev:zm7c15000932@extasea.ds7tsdc.mongodb.net/extasea?retryWrites=true&w=majority")
         console.log("Connect to MongoDB successfully")
     } catch (error) {
         console.log("Connect failed " + error.message )
